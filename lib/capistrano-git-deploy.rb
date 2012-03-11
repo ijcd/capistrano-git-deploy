8e5a55802ce3e4bb00c339a06b7ad853f1bd9dfb
Ian Duggan <ian@ianduggan.net>
2012-03-10 20:46:49 -0800

    Adding fast_git_deploy code

--- lib/capistrano-git-deploy.rb
+++ lib/capistrano-git-deploy.rb
@@ -1,9 +1,12 @@
 require "capistrano-git-deploy/version"
 
-module Capistrano
-  module Git
-    module Deploy
-      # Your code goes here...
-    end
-  end
-end
+# module Capistrano
+#   module Git
+#     module Deploy
+#       # Your code goes here...
+#     end
+#   end
+# end
+
+Capistrano::Configuration.instance.load_paths << File.join(File.dirname(__FILE__), "recipes")
+Capistrano::Configuration.instance.load "fast_git_deploy"
